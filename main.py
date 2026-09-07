0d2417d3d6f053adbb6088d5ab1b78faab91200d
mdsiblen-rgb <mdsiblen@gmail.com>
2026-09-07 19:53:22 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-from flask import Flask, render_template
-import os
-import threading
-from bot import application
-import asyncio
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Bot is Live!"
 
-def run_bot():
-    asyncio.run(application.run_polling())
-
-if __name__ == '__main__':
-    # বট আলাদা থ্রেডে চালু হবে
-    bot_thread = threading.Thread(target=run_bot)
-    bot_thread.start()
-    
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run()
