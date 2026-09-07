85ba1c03af25e15bc9dd286dfc34933751184cff
mdsiblen-rgb <mdsiblen@gmail.com>
2026-09-07 16:41:27 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot চালু আছে! ✅")
+    await update.message.reply_text("Bot চালু আছে ভাই ✅")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
