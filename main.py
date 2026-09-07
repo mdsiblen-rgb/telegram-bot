ab62e057f3e1adacbcb701b3094de2ae1444a311
mdsiblen-rgb <mdsiblen@gmail.com>
2026-09-07 19:04:16 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from flask import Flask, render_template
-import threading
-import asyncio
-import bot
-
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-def run_bot():
-    asyncio.run(bot.application.run_polling())
-
-if __name__ == "__main__":
-    threading.Thread(target=run_bot).start()
-    app.run(host="0.0.0.0", port=10000)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
