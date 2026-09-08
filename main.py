995c5cdfbc05bb8fc9a09897c371f194d97d7014
mdsiblen-rgb <mdsiblen@gmail.com>
2026-09-09 02:48:41 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template
 import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/health')
 def health():
-    return "OK - Live"
+    return "OK"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
-    app
+    app.run(host='0.0.0.0', port=port)
