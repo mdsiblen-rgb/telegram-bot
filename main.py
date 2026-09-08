da5ca88960a443dd95363cbcbc68e1dac731d853
mdsiblen-rgb <mdsiblen@gmail.com>
2026-09-08 14:04:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,11 @@
+from flask import Flask, render_template
 import os
-from flask import Flask
-from threading import Thread
-import telebot
 
-# তোমার বট টোকেন এখানে
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "তোমার টোকেন এখানে বসাও")
-
-bot = telebot.TeleBot(BOT_TOKEN)
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is Live!"
+    return render_template('index.html')
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "Bot Active! ✅")
-
-def run_bot():
-    bot.infinity_polling()
-
-def run_flask():
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
-
-if __name__ == "__main__":
-    Thread(target=run_bot).start()
-    run_flask()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
