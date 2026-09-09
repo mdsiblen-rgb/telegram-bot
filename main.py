0c035bc9739ad2d3657604cc3f5a8179e9bda544
mdsiblen-rgb <mdsiblen@gmail.com>
2026-09-10 02:19:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo, ReplyKeyboardMarkup
+import os
 
 app = Flask(__name__)
+URL = "https://telegram-bot-1-v77g.onrender.com"
 
 @app.route('/')
-@app.route('/<path:path>')
-def home(path=None):
+def home():
     return render_template('index.html')
 
+# এটা তোমার বটের মেনু ঠিক করবে
+def get_main_menu():
+    keyboard = [
+        ["💰 Balance", "👥 My Referrals"],
+        ["🎁 Daily Bonus", "🔗 Refer Link"],
+        ["🌐 Community Task", "🛠️ Admin Panel"]
+    ]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
