b9361ae074912e99a15449007cb2a80293a4bfbf
mdsiblen-rgb <mdsiblen@gmail.com>
2026-09-10 00:28:48 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, ReplyKeyboardMarkup
 from flask import Flask, send_from_directory
 import threading
+import os
 
-BOT_TOKEN = "এখানে তোমার টোকেন বসাও"
+BOT_TOKEN = os.environ.get("BOT_TOKEN") # BotFather থেকে নেওয়া টোকেন
 APP_URL = "https://telegram-bot-1-v77g.onrender.com"
 
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN) # bot আগে বানালাম
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,21 +18,21 @@ def home():
 def start(m):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add("🌐 Community Task", "💰 Balance")
-    bot.send_message(m.chat.id, "বট চালু আছে ✅\nCommunity Task এ ক্লিক করো", reply_markup=markup)
+    bot.send_message(m.chat.id, "বট চালু আছে ✅", reply_markup=markup)
 
-# ✅ এইটা এখন bot এর নিচে আছে, তাই Error হবে না
 @bot.message_handler(func=lambda m: m.text == "🌐 Community Task")
 def community_task(m):
     markup = InlineKeyboardMarkup()
     markup.add(InlineKeyboardButton("🏢 Open Company App", web_app=WebAppInfo(url=APP_URL)))
-    markup.add(InlineKeyboardButton("💲 Direct Link - omg10.com", url="https://omg10.com/4/11760259"))
+    markup.add(InlineKeyboardButton("💲 Direct Link omg10.com", url="https://omg10.com/4/11760259"))
     bot.send_message(m.chat.id, "কোম্পানি অ্যাপ ওপেন করো 👇", reply_markup=markup)
 
 def run_bot():
     bot.infinity_polling()
 
 def run_web():
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     threading.Thread(target=run_bot).start()
