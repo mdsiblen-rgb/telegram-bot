5ffcef47487aa07dded1c4973f66736b59f473b1
mdsiblen-rgb <mdsiblen@gmail.com>
2026-09-15 04:39:15 +0600

    Update mane.py

--- mane.py
+++ mane.py
@@ -6,8 +6,9 @@ DB='database.json'
 
 def load_db():
     d={
-        "app_name":"👑 প্রতিদিনের কাজ বিডি","app_name2":"Daily Work BD ✅","app_logo": 
-        "primary":"#8b5cf6","secondary":"#f59e0b",
+        "app_name":"👑 প্রতিদিনের কাজ বিডি","app_name2":"Daily Work BD ✅",
+"primary":"#8b5cf6","secondary":"#f59e0b",
+
         "bonus":20,"welcome_title":"🎉 স্বাগতম!","welcome_msg":"৳20 বোনাস পেয়েছেন! এখন ইনকাম শুরু করুন!",
         "ad":0.20,"pop":0.30,"clim":50,"plim":30,"min":500,"ref":80,"ad_time":10,"diamond_rate":100,
         "company_ad_id":"11764581","popup_ad_id":"11798857",
